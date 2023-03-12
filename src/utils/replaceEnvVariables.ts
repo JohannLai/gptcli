30b00e4414ec19ca9d3e8900bdb17cb875ff401a
Johannlai <johann_lai@outlook.com>
2023-03-12 18:55:55 +0800

    :bug:

--- src/utils/replaceEnvVariables.ts
+++ src/utils/replaceEnvVariables.ts
@@ -1,6 +1,6 @@
 export function replaceEnvVariables(str: string, env: { [key: string]: string }): string {
 	const reg = /\$(\w+)/g;
 	return str.replace(reg, (match, variableName) => {
-		return env[variableName] || '';
+		return env[variableName];
 	});
 }
