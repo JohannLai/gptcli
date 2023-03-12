761e3d9245df449982fc43d1199813bbfd462829
Johannlai <johann_lai@outlook.com>
2023-03-12 20:21:36 +0800

    :hammer: update ask output

--- src/jobs/gpt/ask.ts
+++ src/jobs/gpt/ask.ts
@@ -2,7 +2,6 @@ import inquirer from 'inquirer';
 import { Base, IConfigBase } from '../base.js';
 import { replaceEnvVariables } from '../../utils/replaceEnvVariables.js';
 
-
 export class Ask extends Base {
 	constructor(args: IConfigBase) {
 		super(args);
