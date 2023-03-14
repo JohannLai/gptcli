29243f439b48646a86a8f49f7a8c513a98d7783b
johannlai <johann_lai@outlook.com>
2023-03-14 16:18:45 +0800

    :recycle: refactored open ai to util

--- src/jobs/gpt/createChatCompletion.ts
+++ src/jobs/gpt/createChatCompletion.ts
@@ -1,8 +1,7 @@
-import axios from 'axios';
 import { Base, IConfigBase } from '../base.js';
 import { replaceEnvVariables } from '../../utils/replaceEnvVariables.js';
+import { createChatCompletion } from '../../utils/openaiAPI/createChatCompletion.js';
 import ora from 'ora';
-import chalk from 'chalk';
 
 export type IMessages = Array<{
 	content: string;
@@ -18,7 +17,6 @@ export class CreateChatCompletion extends Base {
 		super.run();
 
 		const { messages } = this.with as { messages: IMessages };
-		// 给 messages 中的 item 的 content 加上环境变量
 		const messagesWithEnv = messages.map((item) => {
 			return {
 				...item,
@@ -30,35 +28,15 @@ export class CreateChatCompletion extends Base {
 			}
 		});
 
-		// show loading
 		const spinner = ora('AI is thinking...').start();
 
-		const { data } = await axios.post('https://api.openai.com/v1/chat/completions', {
-			model: "gpt-3.5-turbo",
+		const { data } = await createChatCompletion({
 			messages: messagesWithEnv,
-		}, {
-			headers: {
-				'Authorization': `Bearer ${process.env.OPENAI_API_KEY}`,
-				'Content-Type': 'application/json'
-			}
-		}).then(res => res, (err) => {
-			spinner.fail("gitmoji AI is thinking failed");
-			console.error(chalk.red('Error:', err.response.data.error.message));
-			console.error(chalk.reset(`
-	Possible reasons:
-
-	1. My OPENAI_API_KEY is expired, please contact me to charged it.
-	   Or set your own OPENAI_API_KEY, You can get your OPENAI_API_KEY from https://beta.openai.com/account/api-keys
-
-		e.g: gpt config user.OPENAI_API_KEY sk - xxx
-
-	2. The OPENAI_API_KEY is not set, please set it first.
-	3. OPENAI server is down, please try again later.You can check the status from https://status.openai.com/
-	`));
-			return err;
 		}).finally(() => {
 			spinner.stop();
 		});
+
+		spinner.stop();
 
 		if (!data) {
 			process.exit(1);
---
+++ src/utils/openaiAPI/createChatCompletion.ts
@@ -0,0 +1,31 @@
+import axios from "axios";
+import chalk from "chalk";
+
+export async function createChatCompletion(opt: any) {
+	const { data } = await axios.post('https://api.openai.com/v1/chat/completions', {
+		model: "gpt-3.5-turbo",
+		...opt
+	}, {
+		headers: {
+			'Authorization': `Bearer ${process.env.OPENAI_API_KEY}`,
+			'Content-Type': 'application/json'
+		}
+	}).then(res => res, (err) => {
+		console.error(chalk.red('Error:', err.response.data.error.message));
+		console.error(chalk.reset(`
+	Possible reasons:
+
+	1. My OPENAI_API_KEY is expired, please contact me to charged it.
+	   Or set your own OPENAI_API_KEY, You can get your OPENAI_API_KEY from https://beta.openai.com/account/api-keys
+
+		e.g: gpt config user.OPENAI_API_KEY sk - xxx
+
+	2. The OPENAI_API_KEY is not set, please set it first.
+	3. OPENAI server is down, please try again later.You can check the status from https://status.openai.com/
+	`));
+
+		return err;
+	});
+
+	return data;
+}
