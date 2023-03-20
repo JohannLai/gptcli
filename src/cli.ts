3eea74baa02576a29ec61f13f52292f6c7164683
johannlai <johann_lai@outlook.com>
2023-03-20 19:30:28 +0800

    :recycle: Refactor code tab to space

--- src/cli.ts
+++ src/cli.ts
@@ -12,30 +12,30 @@ import { PLUGINS_DIR } from './constants.js';
 const version = await getPackageVersion();
 
 const argv = cli({
-	name: 'gpt-cli',
+  name: 'gpt-cli',
 
-	version: `🔥 v${version}`,
+  version: `🔥 v${version}`,
 
-	parameters: [
-		'<plugin>',
-		'[optional spread...]'
-	],
+  parameters: [
+    '<plugin>',
+    '[optional spread...]'
+  ],
 
-	flags: {},
+  flags: {},
 
-	help: {
-		version: `🔥 v${version}`,
-		description: '✨ ALL IN ONE CLI for ChatGPT command line tool',
-		usage: [
-			'👉 gpt <plugin> [optional spread...]',
-			'👉 gpt list',
-			'👉 gpt config [pluginName/user.key] [value]',
-		],
-		examples: [
-			'🕹️  gpt gitmoji "fix a bug"',
-			'🕹️  gpt config user.OPENAI_API_KEY sk-xxx'
-		],
-	},
+  help: {
+    version: `🔥 v${version}`,
+    description: '✨ ALL IN ONE CLI for ChatGPT command line tool',
+    usage: [
+      '👉 gpt <plugin> [optional spread...]',
+      '👉 gpt list',
+      '👉 gpt config [pluginName/user.key] [value]',
+    ],
+    examples: [
+      '🕹️  gpt gitmoji "fix a bug"',
+      '🕹️  gpt config user.OPENAI_API_KEY sk-xxx'
+    ],
+  },
 });
 
 
@@ -43,68 +43,68 @@ const { plugin, optionalSpread } = argv._;
 
 // List all plugins
 if (argv._[0] == 'list') {
-	const dirname = url.fileURLToPath(new URL('.', import.meta.url))
+  const dirname = url.fileURLToPath(new URL('.', import.meta.url))
 
-	const builtInPlugins = readdirSync(join(dirname, 'plugins')).map((plugin) => plugin.replace('.yml', ''));
+  const builtInPlugins = readdirSync(join(dirname, 'plugins')).map((plugin) => plugin.replace('.yml', ''));
 
-	console.log(`
+  console.log(`
 	${chalk.reset('\n ✨ Built-in Plugins\n   🕹️ ')}${builtInPlugins.join('\n   🕹️ ')}
 	`);
 
-	// get all  folders in plugins dir
-	const InstalledPlugins = readdirSync(PLUGINS_DIR)
+  // get all  folders in plugins dir
+  const InstalledPlugins = readdirSync(PLUGINS_DIR)
 
-	console.log(`
+  console.log(`
 	${chalk.reset('\n ✨ Installed Plugins\n   🕹️ ')}${InstalledPlugins.join('\n   🕹️ ')}
 	`);
 
-	process.exit(0);
+  process.exit(0);
 }
 
 
 // Config plugin
 // TODO: can be refactored to a plugin, use plugin/gpt/config.yml
 if (argv._[0] == 'config') {
-	// e.g: optionalSpread: [ 'user.token', '123' ]
-	const isSet = argv._.optionalSpread.length == 2;
+  // e.g: optionalSpread: [ 'user.token', '123' ]
+  const isSet = argv._.optionalSpread.length == 2;
 
-	if (!isSet) {
-		const [scope, key] = argv._.optionalSpread[0].split('.');
-		const config = getConfigFromGptrc(scope, key);
+  if (!isSet) {
+    const [scope, key] = argv._.optionalSpread[0].split('.');
+    const config = getConfigFromGptrc(scope, key);
 
-		console.log(config);
-		process.exit(0);
-	}
+    console.log(config);
+    process.exit(0);
+  }
 
-	// set config
-	const [scope, key] = argv._.optionalSpread[0].split('.');
-	const value = argv._.optionalSpread[1];
+  // set config
+  const [scope, key] = argv._.optionalSpread[0].split('.');
+  const value = argv._.optionalSpread[1];
 
-	setConfigToGptrc(scope, key, value);
+  setConfigToGptrc(scope, key, value);
 
-	process.exit(0);
+  process.exit(0);
 }
 
 const pluginConfig = await getPluginConfig(plugin);
 
 // Print plugin info
 if (optionalSpread[0] == 'info') {
-	const { name, description, repository } = pluginConfig;
-	console.log(`
+  const { name, description, repository } = pluginConfig;
+  console.log(`
     Name: ${name}
     Description: ${description}
     Repository: ${chalk.underline(`https://github.com/${repository}`)}
     authored by: ${pluginConfig.author}
 `);
 
-	process.exit(0);
+  process.exit(0);
 }
 
 // Print plugin help
 if (optionalSpread[0] == 'help') {
-	console.log(pluginConfig.help);
+  console.log(pluginConfig.help);
 
-	process.exit(0);
+  process.exit(0);
 }
 
 run(pluginConfig, argv)
