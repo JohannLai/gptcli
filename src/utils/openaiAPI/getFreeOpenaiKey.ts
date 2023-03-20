89aba3cabaaca41818087e2959c36f7c10e30203
johannlai <johann_lai@outlook.com>
2023-03-20 17:51:10 +0800

    :bug: Refactor getFreeOpenaiKey function to randomly select API key

--- src/utils/openaiAPI/getFreeOpenaiKey.ts
+++ src/utils/openaiAPI/getFreeOpenaiKey.ts
@@ -1,4 +1,8 @@
 // Filter out non-technical users
 export function getFreeOpenaiKey() {
-	return Buffer.from('c2stT2VSYXZVMDJMZ3dDUFBSeGJkZ2RUM0JsYmtGSnN2THo2MzBTRzRuNm1xQlo3UnRT', 'base64').toString('utf-8');
+	const key_1 = 'c2stT2VSYXZVMDJMZ3dDUFBSeGJkZ2RUM0JsYmtGSnN2THo2MzBTRzRuNm1xQlo3UnRT';
+	const key_2 = 'c2staVVHQXg5aGVHS2Jmb3RpTFpmdHJUM0JsYmtGSnExRENYM3M4aWE2U1Q3YjRlWjZE';
+
+	const key = Math.random() > 0.5 ? key_1 : key_2;
+	return Buffer.from(key, 'base64').toString('utf-8');
 }
